3902c4236b726a1a96d89b03fa1eda39a782aeea
William Lee <karlweiyang@gmail,com>
2019-04-10 10:26:54 +0100

    Updates

--- precache-manifest.b4f5a9d9b9769bc801c0d65bbde82529.js
+++ precache-manifest.b4f5a9d9b9769bc801c0d65bbde82529.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/gifme/static/js/runtime~main.eb951287.js"
   },
   {
-    "revision": "15cf4f26629442df8934",
-    "url": "/gifme/static/js/main.a5bbc599.chunk.js"
+    "revision": "033bc49a5236d95eb0ba",
+    "url": "/gifme/static/js/main.a842c380.chunk.js"
   },
   {
     "revision": "2f73ea753d7706ddce9c",
     "url": "/gifme/static/js/2.66773f37.chunk.js"
   },
   {
-    "revision": "15cf4f26629442df8934",
+    "revision": "033bc49a5236d95eb0ba",
     "url": "/gifme/static/css/main.3497f86d.chunk.css"
   },
   {
-    "revision": "595815bdb8c9ca2eb5da7212d597cf76",
+    "revision": "6a0b6f27bb51dac2e9aff99d86094b75",
     "url": "/gifme/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gifme/precache-manifest.5d026c683654784ee05ea53d9ebb8151.js"
+  "/gifme/precache-manifest.b4f5a9d9b9769bc801c0d65bbde82529.js"
 );
 
 workbox.clientsClaim();
